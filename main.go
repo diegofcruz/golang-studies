10182477d3b8f2382a918ae0dc899f8cfb365e5f
Diego Cruz <dfcruz@Diegos-MacBook-Pro.local>
2026-05-31 23:01:18 -0300

    feat: test import

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	bootcheck "golang-studies/bootcheck"
+)
 
 func main() {
+	// test := 25
+
+	fmt.Println(bootcheck.EnvironmentOK("1.1.1"))
+
 	fmt.Println("go studies ready")
 }
